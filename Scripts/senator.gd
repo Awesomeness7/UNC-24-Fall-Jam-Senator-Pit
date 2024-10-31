6e0cdd5e760c30713a6093690f39cce363f4de88
proprixia <anna@fruitofthefirehose.org>
2024-10-31 17:12:27 -0400

    Reimplemented death after it was broken

--- Scripts/senator.gd
+++ Scripts/senator.gd
@@ -2,28 +2,41 @@ extends RigidBody2D
 
 var deadSenatorSprite = load("res://Sprites/deadSenator.png")
 
-var alive = true
+var state = 0 # 0 = Passive, 1 = dead, 2 = standing up, 3 walking off stage
 var hp = 1.0
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
 
 func die():
 	print("Dieded")
-	alive = false
+	state = 1
 	hp = 0.0
 	get_node("./Sprite2D").texture = deadSenatorSprite
 
-func hitboxIntruded(body: Node2D) -> void:
-	if alive:
-		if (body.has_meta("Lethality")):
-			match body.get_meta("Lethality"):
+var positionAtLastCheck
+func stillnessCheck() -> void:
+	if (state == 0):
+		if ((position == positionAtLastCheck) && (linear_velocity.length() == 0)):
+			print("Raise up!")
+			print(rotation)
+			state = 2
+		else:
+			positionAtLastCheck = position
+
+func hitboxIntruded(intrudingBody: Node2D) -> void:
+	if state != 1:
+		if (intrudingBody.has_meta("Lethality")):
+			print("Lethal intrusion!")
+			match intrudingBody.get_meta("Lethality"):
 				1:
-					hp -= .7
-		if hp <= 0:
-			die()
+					hp -= .34
+					state = 0
+			if hp <= 0:
+				die()
