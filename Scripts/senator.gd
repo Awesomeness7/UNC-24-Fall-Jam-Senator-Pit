3ee6a26199be9970a260bb1719b1fd2e3697e26d
BuildTools <jasonarmbru@yahoo.com>
2024-11-01 01:01:08 -0400

    Damage changes and cleanup

--- Scripts/senator.gd
+++ Scripts/senator.gd
@@ -8,11 +8,14 @@ var hp = 1.0
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	pass # Replace with function body.
+	set_contact_monitor(true)
+	max_contacts_reported = 10
+	
+var old_lin_vel: Vector2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	old_lin_vel = linear_velocity
 
 func die():
 	print("Dieded")
@@ -30,13 +33,14 @@ func stillnessCheck() -> void:
 		else:
 			positionAtLastCheck = position
 
-func hitboxIntruded(intrudingBody: Node2D) -> void:
+
+func collisionBegin(intrudingBody: Node2D) -> void:
 	if state != 1:
 		if (intrudingBody.has_meta("Lethality")):
-			print("Lethal intrusion!")
+			var force = linear_velocity.distance_to((old_lin_vel))
 			match intrudingBody.get_meta("Lethality"):
 				1:
-					hp -= .34
+					hp -= force/1000
 					state = 0
 			if hp <= 0:
 				die()
