704828a9ab808f439aa67d10ff7ce7a22a42a715
proprixia <anna@fruitofthefirehose.org>
2024-11-02 10:36:34 -0400

    Added standing up (work to be done on integrating this with the broader physics world)

--- Scripts/senator.gd
+++ Scripts/senator.gd
@@ -6,22 +6,32 @@ var state = 0 # 0 = Passive, 1 = dead, 2 = standing up, 3 walking off stage
 var hp = 1.0
 
 # Called when the node enters the scene tree for the first time.
+var feetPositionInitial = Vector2(0,0)
+var feetPositionCurrent = Vector2(0,0)
 
 func _ready() -> void:
 	set_contact_monitor(true)
 	max_contacts_reported = 10
-	
+
 var old_lin_vel: Vector2
+
+func beginToStandUp():
+	feetPositionInitial = position + (65/2 - 12) * Vector2(cos(rotation + 3.14159 / 2), sin(rotation + 3.14159/2))
+	feetPositionCurrent = feetPositionInitial
+	# Other code to initiate animation, etc.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	match (state):
-		3:
-			rotation -= sign(rotation) * delta * .2
+		2:
+			rotation -= sign(rotation) * delta
 			
 			if abs(rotation) < .0005:
 				rotation = 0
 				state = 3
+				
+			feetPositionInitial = position + (65/2 - 12) * Vector2(cos(rotation + 3.14159 / 2), sin(rotation + 3.14159/2))
+			position += (feetPositionCurrent - feetPositionInitial)
 
 func die():
 	print("Dieded")
@@ -31,12 +41,10 @@ func die():
 
 var positionAtLastCheck
 func stillnessCheck() -> void:
-	print("Happens")
 	if (state == 0):
 		if ((position == positionAtLastCheck) && (linear_velocity.length() == 0)):
-			print("Raise up!")
-			print(rotation)
 			state = 2
+			beginToStandUp()
 		else:
 			positionAtLastCheck = position
 
@@ -49,8 +57,7 @@ func collisionBegin(intrudingBody: Node2D) -> void:
 			var force = linear_velocity.distance_to((old_lin_vel))
 			match intrudingBody.get_meta("Lethality"):
 				1:
-					hp -= .34
-					hp -= force/1000
+					hp -= force/3000
 					state = 0
 			if hp <= 0:
 				die()
